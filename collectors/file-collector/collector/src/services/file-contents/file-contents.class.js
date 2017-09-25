844cace61d98131fb696ac37526e6a5479e060ea
Zicklag <zicklag@katharostech.com>
2017-09-25 17:36:20 -0500

    Added file-contents service

---
+++ collectors/file-collector/collector/src/services/file-contents/file-contents.class.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+class Service {
+  constructor (options) {
+    this.options = options || {};
+    this.data = {};
+  }
+
+  find (params) {
+    return Promise.resolve(this.data);
+  }
+
+  create (data, params) {
+    this.data = data;
+    return Promise.resolve(data);
+  }
+}
+
+module.exports = function (options) {
+  return new Service(options);
+};
+
+module.exports.Service = Service;
--- collectors/file-collector/collector/src/services/index.js
+++ collectors/file-collector/collector/src/services/index.js
@@ -1,5 +1,7 @@
 const collector = require('./collector/collector.service.js');
+const fileContents = require('./file-contents/file-contents.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(collector);
+  app.configure(fileContents);
 };
