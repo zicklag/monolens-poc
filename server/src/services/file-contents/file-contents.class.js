0309e566353936ddb0894aafe2f951327b47f6e6
Zicklag <zicklag@katharostech.com>
2017-09-25 21:37:25 -0500

    Implemented file-contents service

--- server/src/services/file-contents/file-contents.class.js
+++ server/src/services/file-contents/file-contents.class.js
@@ -2,36 +2,16 @@
 class Service {
   constructor (options) {
     this.options = options || {};
+    this.data = {}
   }
 
   find (params) {
-    return Promise.resolve([]);
-  }
-
-  get (id, params) {
-    return Promise.resolve({
-      id, text: `A new message with ID: ${id}!`
-    });
+    return Promise.resolve(this.data);
   }
 
   create (data, params) {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current)));
-    }
-
-    return Promise.resolve(data);
-  }
-
-  update (id, data, params) {
-    return Promise.resolve(data);
-  }
-
-  patch (id, data, params) {
-    return Promise.resolve(data);
-  }
-
-  remove (id, params) {
-    return Promise.resolve({ id });
+    this.data = data
+    return Promise.resolve(this.data);
   }
 }
 
